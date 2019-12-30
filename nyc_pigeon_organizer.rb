d3293dc23b44c1d8f715ce8ff5ee7594c8b8d0ed
Christine Doherty <christineadoherty@gmail.com>
2019-12-30 21:11:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,13 @@
 def nyc_pigeon_organizer(data)
   updated_pigeon_hash = {}
   updated_pigeon_hash = {
-    :Theo => {},
+    :"Alex" => {},
+    :"Theo" => {},
     :"Peter Jr." => {},
-    :"Lucky" => {},
-    :"Ms. K" => {},
-    :"Queenie" => {},
     :"Andrew" => {},
-    :"Alex" => {}
+    :"Lucky" => {},
+    :"Queenie" => {},
+    :"Ms. K" => {}
   }
  
 end
